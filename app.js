9245f5250f88403a320ccc0236e0f1112005669a
homan05872 <129469204+homan05872@users.noreply.github.com>
2023-07-09 18:35:58 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var circle = document.getElementById("circle");
+// console.log(circle)
+var upBtn = document.getElementById("upBtn");
+var downBtn = document.getElementById("downBtn");
+
+var rotateValue = circle.style.transform;
+// console.log(rotateValue)
+var rotateSum;
+
+upBtn.onclick = function () {
+    // 時計回りに90°回転させる
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+};
+
+downBtn.onclick = function () {
+    // 半時計回りに90°回転させる
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
